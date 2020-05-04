255d8acc7451095f93a8fb7f37fb02cd0627e78e
lishan1047 <lishan_work@126.com>
2020-05-04 11:19:17 +0800

    First commit.

---
+++ eIvy.WebService.Client/AccessToken.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace eIvy.WebService.Client
+{
+    /// <summary>
+    /// Access token for the eIvy Web Service。
+    /// </summary>
+    public class AccessToken
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public AccessToken()
+        {
+            this.CreatedTime = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Get the created time of the access token.
+        /// </summary>
+        [Newtonsoft.Json.JsonIgnore]
+        public DateTime CreatedTime
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Get the expired time of the access token.
+        /// </summary>
+        [Newtonsoft.Json.JsonIgnore]
+        public DateTime ExpiredTime
+        {
+            get
+            {
+                return this.CreatedTime.AddMinutes(this.ExpiredIn);
+            }
+        }
+
+        /// <summary>
+        /// Get or set the open id of the access token.
+        /// </summary>
+        public string OpenID
+        { get; set; }
+
+        /// <summary>
+        /// Get or set the expired minutes from the created time of the access token.
+        /// </summary>
+        public int ExpiredIn
+        { get; set; }
+    }
+}
---
+++ eIvy.WebService.Client/ServiceRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace eIvy.WebService.Client
+{
+    /// <summary>
+    /// Request the service from eIvy platform applications.
+    /// </summary>
+    public class ServiceRequest
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="baseUrl">The request base url.</param>
+        public ServiceRequest(string baseUrl)
+        {
+            this.BaseUrl = baseUrl;
+        }
+        /// <summary>
+        /// Get the basic URL of requesting services.
+        /// </summary>
+        public string BaseUrl
+        {
+            get;
+            private set;
+        }
+    }
+}
